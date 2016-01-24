a9967540951fe7f57fa182703caab34258d9d286
lorenzo-carbonell <atareao_r@raring>
2016-01-24 09:37:50 +0100

    Added multithread and daemons

--- src/crushingmachine.py
+++ src/crushingmachine.py
@@ -29,6 +29,8 @@ from gi.repository import Gdk
 from gi.repository import GObject
 from gi.repository import Nautilus
 from gi.repository import Unity
+import urllib
+import urllib.request
 import shutil
 import time
 import sys
@@ -40,9 +42,11 @@ import subprocess
 import functools
 import concurrent
 import concurrent.futures
+import threading
+import queue
 
 NUM_THREADS = 4
-
+GObject.threads_init()
 
 try:
 	current_locale, encoding = locale.getdefaultlocale()
@@ -66,6 +70,35 @@ def wait(time_lapse):
 		while Gtk.events_pending():
 			Gtk.main_iteration()
 
+class Worker(GObject.GObject,threading.Thread):
+	__gsignals__ = {
+		'task-done':(GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE,(object,)),
+		}
+	
+	def __init__(self,cua,secureDelete,total):
+		threading.Thread.__init__(self)
+		GObject.GObject.__init__(self)		
+		self.setDaemon(True)
+		self.cua = cua
+		self.sd = secureDelete
+		self.total = total
+
+	def run(self):
+		while True:
+			afile = self.cua.get()
+			if afile is None or self.sd.stop:
+				break
+			if afile!=None and os.path.exists(afile):
+				args = ['srm','-r', afile]
+				print(_('Crushing')+' %s'%afile)
+				subprocess.call(args)
+				print(_('Crushed')+' %s'%afile)
+			count = self.sd.launcher.get_property("count")-1
+			self.sd.launcher.set_property("count",count)
+			self.sd.launcher.set_property("progress",(self.total-count)/self.total)
+			self.emit('task-done',afile)
+			self.cua.task_done()
+
 class SecureDelete(Gtk.Dialog): # needs GTK, Python, Webkit-GTK
 	def __init__(self,files = None):
 		#***************************************************************
@@ -88,11 +121,25 @@ class SecureDelete(Gtk.Dialog): # needs GTK, Python, Webkit-GTK
 		frame2.add(hbox21)
 		#
 		button20 = Gtk.Button()
-		button20.set_image(Gtk.Image.new_from_stock(Gtk.STOCK_DIRECTORY,Gtk.IconSize.BUTTON))
+		button20.set_image(Gtk.Image.new_from_stock(Gtk.STOCK_ADD,Gtk.IconSize.BUTTON))
 		button20.set_size_request(32,32)
-		button20.set_tooltip_text(_('Load files'))
+		button20.set_tooltip_text(_('Add files'))
 		button20.connect('clicked',self.on_button_load_clicked)
 		hbox21.pack_start(button20,False,False,0)			
+		#
+		button5 = Gtk.Button()
+		button5.set_image(Gtk.Image.new_from_stock(Gtk.STOCK_REMOVE,Gtk.IconSize.BUTTON))
+		button5.set_size_request(32,32)
+		button5.set_tooltip_text(_('Remove selected files'))
+		button5.connect('clicked',self.on_button_remove_one_file)
+		hbox21.pack_start(button5,False,False,0)	
+		#
+		button4 = Gtk.Button()
+		button4.set_image(Gtk.Image.new_from_stock(Gtk.STOCK_CLOSE,Gtk.IconSize.BUTTON))
+		button4.set_size_request(32,32)
+		button4.set_tooltip_text(_('Remove all files'))
+		button4.connect('clicked',self.on_button_remove_seleccion)
+		hbox21.pack_start(button4,False,False,0)	
 		#
 		self.label = Gtk.Label()
 		hbox21.pack_start(self.label,False,False,0)
@@ -124,6 +171,19 @@ class SecureDelete(Gtk.Dialog): # needs GTK, Python, Webkit-GTK
 		model = Gtk.ListStore(str)
 		self.treeview.set_model(model)
 		#
+		# set icon for drag operation
+		self.treeview.connect('drag-begin', self.drag_begin)
+		self.treeview.connect('drag-data-get', self.drag_data_get_data)
+		self.treeview.connect('drag-data-received',self.drag_data_received)
+		#
+		dnd_list = [Gtk.TargetEntry.new('text/uri-list', 0, 100),Gtk.TargetEntry.new('text/plain', 0, 80)]
+		self.treeview.drag_source_set(Gdk.ModifierType.BUTTON1_MASK, dnd_list, Gdk.DragAction.COPY)
+		self.treeview.drag_source_add_uri_targets()
+		dnd_list = Gtk.TargetEntry.new("text/uri-list", 0, 0)
+		self.treeview.drag_dest_set(Gtk.DestDefaults.MOTION | Gtk.DestDefaults.HIGHLIGHT | Gtk.DestDefaults.DROP,[dnd_list],Gdk.DragAction.MOVE )
+		self.treeview.drag_dest_add_uri_targets()
+		
+		#
 		column = Gtk.TreeViewColumn(_('To Secure Delete'),Gtk.CellRendererText(),text=0)
 		self.treeview.append_column(column)		
 		#
@@ -134,7 +194,31 @@ class SecureDelete(Gtk.Dialog): # needs GTK, Python, Webkit-GTK
 		#
 		for afile in files:
 			model.append([afile])
+			
+	def on_button_remove_one_file(self,widget):
+		(model, pathlist) = self.treeview.get_selection().get_selected_rows()
+		for path in pathlist :
+			tree_iter = model.get_iter(path)
+			model.remove(tree_iter)
+					
+	def on_button_remove_seleccion(self,widget):
+		model = self.treeview.get_model()
+		model.clear()
+		
+	def drag_begin(self, widget, context):
+		pass
 
+	def drag_data_get_data(self, treeview, context, selection, target_id, etime):
+		pass
+
+	def drag_data_received(self,widget, drag_context, x, y, selection_data, info, timestamp):
+		model = self.treeview.get_model()
+		for filename in selection_data.get_uris():
+			if len(filename)>8:
+				filename = urllib.request.url2pathname(filename)
+				filename = filename[7:]
+				model.append([filename])
+				
 	def on_button_load_clicked(self,widget):
 		dialog = Gtk.FileChooserDialog(_('Select one or more files to secure delete'),
 										self,
@@ -153,10 +237,8 @@ class SecureDelete(Gtk.Dialog): # needs GTK, Python, Webkit-GTK
 			filenames = dialog.get_filenames()
 			if len(filenames)>0:
 				model = self.treeview.get_model()
-				#model.clear()
 				for filename in filenames:
 					if os.path.isfile(filename) and not self.is_file_in_model(filename):
-						#head,tail = os.path.split(filename)
 						model.append([filename])	
 				self.file_dir = os.path.dirname(filenames[0])
 				if len(self.file_dir)<=0 or os.path.exists(self.file_dir)==False:
@@ -175,47 +257,57 @@ class SecureDelete(Gtk.Dialog): # needs GTK, Python, Webkit-GTK
 		self.get_root_window().set_cursor(Gdk.Cursor(Gdk.CursorType.ARROW))			
 		while Gtk.events_pending():
 			Gtk.main_iteration()		
-		
+	
+	def remove_item(self,worker,afile):
+		model = self.treeview.get_model()
+		aiter = model.get_iter_first()
+		while(aiter is not None):
+			if model[aiter][0] == afile:
+				model.remove(aiter)
+				while Gtk.events_pending():
+					Gtk.main_iteration()		
+			aiter = model.iter_next(aiter)
 	def on_button_secure_delete_clicked(self,widget):
 		self.set_wait_cursor()
+		self.stop = False
 		model = self.treeview.get_model()
 		total=len(model)
 		#			
 		self.launcher.set_property("count", total)
 		self.launcher.set_property("count_visible", True)
 		self.launcher.set_property("progress", 0.0)
-		self.launcher.set_property("progress_visible", True)		
-		with concurrent.futures.ProcessPoolExecutor(max_workers=NUM_THREADS) as executor:
-			self.stop = False
-			for row in model:
-				afile = row[0]
-				cmd = 'srm -r'
-				print(_('Crushing')+' %s'%afile)
-				if len(afile)>50:
-					num = len(afile)-47
-					text = '...'+afile[num:]
-				else:
-					text = afile				
-				future = executor.submit(ejecuta,cmd,[afile])
-				future.add_done_callback(functools.partial(self.callback,total,text,row.iter))
-				if self.stop:
-					break
+		self.launcher.set_property("progress_visible", True)
+		cua = queue.Queue(maxsize=total+2)
+		workers = []
+		print(1)
+		total_workers = total if NUM_THREADS > total else NUM_THREADS
+		for i in range(total_workers):
+			worker = Worker(cua,self,total)
+			worker.connect('task-done',self.remove_item)
+			worker.start()
+			workers.append(worker)
+		print(2)
+		for row in model:
+			afile = row[0]
+			cua.put(afile)
+		# block until all tasks are done
+		print(3)
+		cua.join()
+		# stop workers
+		print(4)
+		for i in range(total_workers):
+			cua.put(None)
+		for worker in workers:
+			worker.join()
+			while Gtk.events_pending():
+				Gtk.main_iteration()
+			
+		print(5)
 		self.label.set_text('')
 		self.launcher.set_property("count_visible", False)
 		self.launcher.set_property("progress_visible", False)
 		self.set_normal_cursor()
 
-	def callback(self,total,text,aiter,job):
-		model = self.treeview.get_model()
-		max_value=len(model)
-		model.remove(aiter)
-		self.label.set_text(_('Crushing')+' %s'%text)
-		self.launcher.set_property("count", total-(max_value-1))
-		self.launcher.set_property("progress", float(total-(max_value-1))/float(total))
-		while Gtk.events_pending():
-			Gtk.main_iteration()
-		print(job.result())
-		
 	def on_button_stop_deletion_clicked(self,widget):
 		self.stop = True
 
@@ -226,14 +318,6 @@ class SecureDelete(Gtk.Dialog): # needs GTK, Python, Webkit-GTK
 				return True
 		return False
 
-def ejecuta(comando,afile=None):
-	args = shlex.split(comando)
-	if afile!=None:
-		args.extend(afile)
-	p = subprocess.Popen(args, bufsize=10000, stdout=subprocess.PIPE)
-	valor = p.communicate()[0]
-	return valor
-	
 def get_files_to_secure_delete(files):
 	files_to_secure_delete = []
 	for afile in files:
